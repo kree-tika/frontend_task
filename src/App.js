4f1c109f78ece16a9cef2484dff2eec5a84d64df
kreetika20 <019bim020@sxc.edu.np>
2023-02-15 11:42:11 +0545

    Revised and done protected routes and redirecting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Views from "./Views";
-import { createContext, useState } from "react";
-import LogInButtons from "./LogInButtons";
+// import { createContext, useState } from "react";
+// import LogInButtons from "./LogInButtons";
 
-export const UserContext = createContext();
+// export const UserContext = createContext();
 
 function App() {
-  const [user, setUser] = useState({ loggedIn: false });
+  // const [user, setUser] = useState({ loggedIn: false });
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <LogInButtons />
-      <Views />
-    </UserContext.Provider>
+    // <UserContext.Provider value={{ user, setUser }}>
+    //   <LogInButtons />
+    <Views />
+    // </UserContext.Provider>
   );
 }
 export default App;
--- src/ProtectedRoutes.js
+++ src/ProtectedRoutes.js
@@ -1,20 +1,26 @@
-import { useContext } from "react";
-import { useLocation } from "react-router";
+// import { useContext } from "react";
+// import { useLocation } from "react-router";
 import { Navigate, Outlet } from "react-router-dom";
-import { UserContext } from "./App";
+// import { UserContext } from "./App";
 
+// const useAuth = () => {
+//   const { user } = useContext(UserContext);
+//   return user && user.loggedIn;
+// };
 const useAuth = () => {
-  const { user } = useContext(UserContext);
+  const user = { loggedIn: false };
+  //const user ={loggedIn:true}; for accessing protected routes
   return user && user.loggedIn;
 };
 
 const ProtectedRoutes = () => {
-  const location = useLocation();
+  // const location = useLocation();
   const isAuth = useAuth();
   return isAuth ? (
     <Outlet />
   ) : (
-    <Navigate to="/" replace state={{ from: location }} />
+    <Navigate to="/" />
+    // replace state={{ from: location }}
   );
 };
 
